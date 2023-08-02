b7790f99bf4caf7556a1c99fd1c4e082af7a915b
Karpenko21 <irinakrivosheeva55@gmail.com>
2023-08-02 23:03:59 +0600

    Sprint 1 hw4

--- src/HW/2/HW2.tsx
+++ src/HW/2/HW2.tsx
@@ -47,6 +47,7 @@ export const HW2 = () => {
   let [currentUsers, setCurrentUsers] = useState<UsersObjectType>(users);
 
   const filterUsers = () => {
+    console.log("yes")
     const filteredUsers =  users.myFriends.filter(f => f.address.city === 'Los Angeles')
     setCurrentUsers({  myFriends: filteredUsers });
   };
