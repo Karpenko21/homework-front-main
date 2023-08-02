1e61c79ac9ffdde53c1839ec9c44c8e5098610c4
Karpenko21 <irinakrivosheeva55@gmail.com>
2023-08-02 23:18:36 +0600

    Sprint 1 hw3

--- src/HW/3/HW3.tsx
+++ src/HW/3/HW3.tsx
@@ -20,7 +20,7 @@ export const HW3 = () => {
   };
 
   const handleSave = () => {
-    setTexts([...texts, currentText])
+    setTexts([ currentText, ...texts])
     setCurrentText("")
     // ЗАСЕТАТЬ БЫ ТЕКСТ В texts И НЕ ПОТЕРЯТЬ НАПУТСТВИЕ ИЗ ПРОШЛОГО ВЕКА)
     // А ЗАТЕМ УБРАТЬ ЗА СОБОЙ В currentText
@@ -29,14 +29,14 @@ export const HW3 = () => {
   return (
     <div id={'hw03'}>
       {currentText ? (
-        <h1 id={'hw03-text'}>ЗДЕСЬ ХОТЕЛОСЬ БЫ УВИДЕТЬ ВВОДИМЫЙ ТЕКСТ</h1>
+        <h1 id={'hw03-text'}>{currentText}</h1>
       ) : (
         <h1 id={'hw03-default-text'}>Здесь появится новое дело</h1> // ничего не меняем, здесь все норм
       )}
 
       <input id={'hw03-input'} type="text" value={currentText} onChange={handleChange} />
 
-      <button id={'hw03-button'} onClick={handleSave}> // НЕ ХВАТАТЕТ ФУНКЦИИ
+      <button id={'hw03-button'} onClick={handleSave}>
         Сохранить
       </button>
 
